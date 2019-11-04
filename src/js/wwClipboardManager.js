7c7422fec232f1759f9d83d948448fb17fb735d2
Guillaume Terrain <gterrain@gmail.com>
2019-11-04 16:05:12 +0100

    Fix paste // word foireux

--- src/js/wwClipboardManager.js
+++ src/js/wwClipboardManager.js
@@ -121,7 +121,7 @@ class WwClipboardManager {
     const textNodes = domUtils.getAllTextNode(node);
 
     textNodes.forEach((textNode) => {
-      if (/\n/.test(textNode.nodeValue)) {
+      if (/\n/.test(textNode.nodeValue) && textNode.parentNode) {
         textNode.parentNode.innerHTML = textNode.nodeValue.replace(/\n/g, '<br>');
       }
     });
